f003ac4465fd0cbf97b537b19c770c708a007233
bagasm24 <mbagas368@gamil.com>
2023-11-21 15:41:55 +0700

    first commit dan instalasi dependencies

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express()
+
+
+
+
+const PORT = process.env.PORT || 3000
+
+
+app.use(express.json())
+
+
+
+app.listen(PORT, () => {
+    console.log("Server Berjalan di PORT " + PORT);
+})
